40cc79424f1b9ea41bc4e470bba2af714ff486ec
Adam Robson <adamray312@gmail.com>
2022-11-01 15:11:13 -0700

    write out the test and the zodiac.js .get for returning a horoscope after adding the horoscopes to the data.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,7 +19,21 @@ describe('zodiac routes', () => {
       name: 'aquarius',
       dates: 'Jan 21 - Feb 19',
       symbol: 'Water Bearer',
+      horoscope: {
+        '2022.09.01': 'Going for your major life goals could be a compelling option. As productive as that would be, you may worry that your progress is distancing you from your peers. If the unspoken tension gets to be too much, clearing the air isn\'t a bad idea.You\'re equipped to express yourself in a way that clarifies your motivations as something positive for yourself, not as a slam against anyone else. While bluntly stating the truth can feel awkward, it usually beats stewing in silence!'
+      }
     };
     expect(res.body).toEqual(sign);
+  });
+
+  it('/zodiac/horoscope/:name should return zodiac sign\'s horoscope', async () => {
+    const res = await request(app).get('/zodiac/horoscope/aquarius');
+    const sign = {
+      name: 'aquarius',
+      horoscope: {
+        '2022.09.01': 'Going for your major life goals could be a compelling option. As productive as that would be, you may worry that your progress is distancing you from your peers. If the unspoken tension gets to be too much, clearing the air isn\'t a bad idea.You\'re equipped to express yourself in a way that clarifies your motivations as something positive for yourself, not as a slam against anyone else. While bluntly stating the truth can feel awkward, it usually beats stewing in silence!'
+      }
+    };
+    expect(res.body).toStrictEqual(sign);
   });
 });
