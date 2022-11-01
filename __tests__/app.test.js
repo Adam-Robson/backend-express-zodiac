7f5c8641c4ebf17c297a1baf8201b5b418f24083
Adam Robson <adamray312@gmail.com>
2022-11-01 10:16:49 -0700

    write out test for each sign using variables and string interpolation

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,4 +10,15 @@ describe('zodiac routes', () => {
     });
     expect(res.body).toEqual(expected);
   });
+
+  it('/signs/:id should return sign detail', async () => {
+    const res = await request(app).get(`/zodiac/:${sign.id}`);
+    const sign = {
+      id: sign.id,
+      name: sign.name,
+      dates: sign.dates,
+      symbol: sign.symbol,
+    };
+    expect(res.body).toEqual(sign);
+  });
 });
